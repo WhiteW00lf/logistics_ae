aa037034f852f09d88eb4faf8f9302b5c7df0bff
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-01-05 08:27:50 +0530

    Fix schema strategy and build int_order_shipments in intermediate layer

---
+++ models/intermediate/int_order_shipments.sql
@@ -0,0 +1,13 @@
+
+SELECT s.shipment_id,
+         s.order_id,
+         s.hub_id,
+         s.shipped_date,
+         s.delivered_date,
+         o.order_date,
+         o.customer_id,
+         o.order_value,
+         o.payment_type
+FROM {{ref('stg_shipments')}} AS s
+LEFT JOIN {{ref('stg_orders')}} AS o
+ON s.order_id = o.order_id
